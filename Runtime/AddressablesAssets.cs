5ff7300cf8042444c3a37dc74fb9de8082d7de66
HQ\alex.oh <alez.oh@nsuslab.com>
2022-11-16 13:02:56 +0900

    Addressables 서포트 기능 추가

---
+++ Runtime/AddressablesAssets.cs
@@ -0,0 +1,11 @@
+#if PACKAGE_ADDRESSABLES
+using System;
+
+namespace beio.Addressable
+{
+    [Serializable]
+    public class AddressablesAssets : Data.AddressableData<UnityEngine.Object>
+    {
+    }
+}
+#endif
